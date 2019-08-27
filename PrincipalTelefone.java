a5e66efa2c186f8a579e4936e9c7494d95189988
mateusgalves <47868761+mateusgalves@users.noreply.github.com>
2019-08-27 19:18:28 -0300

    Update PrincipalTelefone.java

--- PrincipalTelefone.java
+++ PrincipalTelefone.java
@@ -9,35 +9,13 @@ import java.util.Scanner;
 public class PrincipalTelefone {
 
     public static void main(String[] args) {
-        //Telefone p = new Telefone();
-        //TelefoneFixoFio f1 = new TelefoneFixoFio();
-        //TelefoneCelular f2 = new TelefoneCelular();
-        //TelefoneFixoSemFio f3 = new TelefoneFixoSemFio();
         
+     
         TelefoneFixoFio f1 = new TelefoneFixoFio("Lenovo", "X","7", "azul",1,"CAT6" );
 
         f1.ImprimeDados();
-       /** Scanner ler = new Scanner(System.in);
-        System.out.println(": ");
-        p.(ler.nextLine());
-        ler = new Scanner(System.in);
-
-        System.out.println("Entre com a matrícula: ");
-        p.setNumeroMatricula(ler.nextInt());
-
-        System.out.println("Salario Mensal: ");
-        f1.setSalarioMensal(ler.nextDouble());
-
-        System.out.println("Valor Hora: ");
-        f2.setValorHora(ler.nextDouble());
-
-        System.out.println("Horas Trabalhadas: ");
-        f2.setHorasTrabalhadas(ler.nextInt());
-
-        p.ImprimeDados();
-        f1.ImprimeDados();
-        f2.ImprimeDados();
-        */
+        
+     
 
     }
 
