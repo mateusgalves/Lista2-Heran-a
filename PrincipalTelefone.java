2ec5770bb55ee2ba14da0d4a8c255d72ac70ff77
mateusgalves <47868761+mateusgalves@users.noreply.github.com>
2019-08-27 19:28:50 -0300

    Update PrincipalTelefone.java

--- PrincipalTelefone.java
+++ PrincipalTelefone.java
@@ -9,14 +9,19 @@ import java.util.Scanner;
 public class PrincipalTelefone {
 
     public static void main(String[] args) {
-        
-     
-        TelefoneFixoFio f1 = new TelefoneFixoFio("Lenovo", "X","7", "azul",1,"CAT6" );
+        //Telefone p = new Telefone();
+        //TelefoneFixoFio f1 = new TelefoneFixoFio();
+        //TelefoneCelular f2 = new TelefoneCelular();
+        //TelefoneFixoSemFio f3 = new TelefoneFixoSemFio();
 
+        TelefoneFixoFio f1 = new TelefoneFixoFio("Lenovo", "X", "7", "azul", 1, "CAT6");
+        TelefoneCelular f2 = new TelefoneCelular("Lenovo", "X", "7", "azul", "touch", "litio", "alto falante", "FM", "Sim");
+        TelefoneFixoSemFio f3 = new TelefoneFixoSemFio("Lenovo", "X", "7", "azul", "150m", "Bina");
         f1.ImprimeDados();
-        
-     
+        f2.ImprimeDados();
+        f3.ImprimeDados();
 
+       
     }
 
 }
